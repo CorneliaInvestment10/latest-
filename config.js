6629d7d1e1ea9944681db95c18f5dcd5f101afd3
CorneliaInvestment10 <info@corneliainvestmentgroup.org>
2026-05-27 15:53:50 -0400

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// Replace this later with your NBC trackable link.
-// Example: window.NBC_TRACKABLE_LINK = https://www.nationalbusinesscapital.com/apply-now/?ref=140622073002
-window.NBC_TRACKABLE_LINK = https://www.nationalbusinesscapital.com/apply-now/?ref=140622073002
